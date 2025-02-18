35c88de3893fa714709b715e7c76581b1ed39940
Sonali-Rawat <programmergithub789@gmail.com>
2025-02-18 11:35:56 +0530

    add dotenv file and port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const fs= require('fs');
-
+require('dotenv').config();
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
@@ -73,5 +73,6 @@ app.post("/createhisaab", function(req, res){
         });
 } );
 
-app.listen(3000);
+const port = process.env.PORT || 3000; 
+console.log(`Server running on port ${port}`);
 
